fc035b0850f01ba62c7101f8f684bb74652b2b02
Kevin Phung <kevin.phung@nagrastar.com>
2017-09-30 01:02:10 -0600

    refactor: React Native

--- src/containers/DiceBox.js
+++ src/containers/DiceBox.js
@@ -1,25 +1,23 @@
 import React, { PureComponent } from 'react';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import bindAll from 'lodash/bindAll';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import AddBox from 'material-ui/svg-icons/content/add-box';
-import SubtractBox from 'material-ui/svg-icons/toggle/indeterminate-check-box';
-import Switch from '../components/Switch';
+import SquareButton from '../components/SquareButton';
 import NumberSelect from '../components/NumberSelect';
-import { types as dice, rollDieTypeNTimes } from '../modules/dice';
+import { types as dice, rollDieTypeNTimes } from '../models/dice';
 
-const style={
-    margin: '.5em'
-};
-const iconStyle={
-    width: 50,
-    height: 50
-};
+const renderDice = (dice) =>
+    dice.map((die) =>
+        <SquareButton
+            key={ die }
+            label={ die }
+            onClick={ () => rollDice(die) }
+        />
+    )
 
 export default class DiceBox extends PureComponent {
     static displayName = 'DiceBox';
     static propTypes = {
-        diceTypes: PropTypes.arrayOf(PropTypes.string),
         rollDiceCallback: PropTypes.func.isRequired
     }
 
@@ -28,14 +26,12 @@ export default class DiceBox extends PureComponent {
 
         this.state = {
             nDice: 1,
-            modifier: 0,
-            operator: '+'
+            modifier: 0
         };
 
         bindAll(this, [
             'updateDiceCount',
             'updateModifier',
-            'updateOperator',
             'rollDice'
         ]);
     }
@@ -48,74 +44,50 @@ export default class DiceBox extends PureComponent {
         this.setState({ ...this.state, modifier: n });
     }
 
-    updateOperator (switchState) {
-        this.setState({
-            ...this.state,
-            operator: switchState ? '-'  : '+'
-        });
-    }
-
     rollDice (die) {
         const { rollDiceCallback } = this.props;
-        const { nDice, operator, modifier } = this.state;
+        const { nDice, modifier } = this.state;
         const rolls = rollDieTypeNTimes(nDice, dice[die]);
-        const mod = (operator === '-' ? -1 : 1) * modifier;
 
         const results = {
             die,
             rolls,
+            modifier,
             n: nDice,
-            modifier: mod,
-            total: rolls.reduce((sum, val) => sum + val, 0) + mod
+            total: rolls.reduce((sum, val) => sum + val, 0) + modifier
         };
 
         rollDiceCallback(results);
     }
 
     render () {
-        const { diceTypes } = this.props;
         const { updateDiceCount, updateModifier, updateOperator, rollDice } = this;
 
         return (
-            <div style={ style }>
+            <View>
                 <NumberSelect
                     id="dice-count"
                     labelText="n"
                     labelFixed={ true }
                     min={ 1 }
                     max={ 100 }
-                    style={ style }
                     onChange={ updateDiceCount }
                 />
-                { diceTypes &&
-                    diceTypes.map((die) =>
-                        <FloatingActionButton
-                            style={{ ...style, verticalAlign: 'text-bottom' }}
-                            key={ die }
-                            mini={ true }
-                            onClick={ () => rollDice(die) }
-                        >
-                        { die }
-                        </FloatingActionButton>
-                    )
-                }
-                <Switch
-                    id="mod-toggle"
-                    OnIcon={ <SubtractBox style={ iconStyle } /> }
-                    OffIcon={ <AddBox style={ iconStyle } /> }
-                    style={ style }
-                    onClick={ updateOperator }
-                />
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                { renderDice(['d2','d4','d6','d8']) }
+                </View>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                { renderDice(['d10','d12','d20','d100']) }
+                </View>
                 <NumberSelect
                     id="modifier"
                     labelText="modifier"
                     labelFixed={ true }
-                    min={ 0 }
+                    min={ -99 }
                     max={ 100 }
-                    style={ style }
                     onChange={ updateModifier }
                 />
-            </div>
+            </View>
         );
     }
 }
