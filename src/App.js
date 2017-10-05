f473138a65b93214816ef2f03d23d6ccdc951250
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-05 16:03:41 -0600

    refactor: test render the LogView component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import {
   Text,
   View
 } from 'react-native';
-import SquareButton from './components/SquareButton';
-import NumberSelect from './components/NumberSelect';
 import DiceBox from './containers/DiceBox';
+import LogView from './components/LogView';
+
 
 export default class App extends Component {
     render () {
+        const items = [{
+            message: 'You rolled 3d6 for 18 [6, 6, 6]'
+        }, {
+            message: 'You rolled 2d12 for 24 [12, 12]'
+        }, {
+            message: 'You rolled 2d8 for 16 [8, 8]'
+        }];
+
         return (
             <View style={{ marginTop: 60 }}>
-                <DiceBox rollDiceCallback={() => console.log('roll dice')} />
+                <LogView items={ items } />
+                {/* <DiceBox rollDiceCallback={() => console.log('roll dice')} /> */}
             </View>
         );
     }
