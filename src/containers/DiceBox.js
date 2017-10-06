f348febf8f3d4dcc7c7ebd422499806a1543a052
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-06 17:49:52 -0600

    refactor: fix up the styling and make renderDice a class method
    renderDice needs to be a class method so that it can access the rollDice method

--- src/containers/DiceBox.js
+++ src/containers/DiceBox.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import bindAll from 'lodash/bindAll';
 import SquareButton from '../components/SquareButton';
@@ -18,7 +18,8 @@ const renderDice = (dice) =>
 export default class DiceBox extends PureComponent {
     static displayName = 'DiceBox';
     static propTypes = {
-        rollDiceCallback: PropTypes.func.isRequired
+        rollDiceCallback: PropTypes.func.isRequired,
+        style: PropTypes.object,
     }
 
     constructor (props) {
@@ -32,7 +33,8 @@ export default class DiceBox extends PureComponent {
         bindAll(this, [
             'updateDiceCount',
             'updateModifier',
-            'rollDice'
+            'rollDice',
+            'renderDice'
         ]);
     }
 
@@ -60,34 +62,67 @@ export default class DiceBox extends PureComponent {
         rollDiceCallback(results);
     }
 
+    renderDice (dice) {
+        return dice.map((die) =>
+            <SquareButton
+                key={ die }
+                label={ die }
+                onPress={ () => this.rollDice(die) }
+            />
+        );
+    }
+
     render () {
         const { updateDiceCount, updateModifier, updateOperator, rollDice } = this;
 
         return (
-            <View>
+            <View style={ styles.container }>
                 <NumberSelect
                     id="dice-count"
-                    labelText="n"
-                    labelFixed={ true }
                     min={ 1 }
                     max={ 100 }
                     onChange={ updateDiceCount }
+                    style={ styles.selectors }
+                    itemStyle={ styles.selectorItem }
                 />
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                { renderDice(['d2','d4','d6','d8']) }
+                <View style={ styles.buttonContainers }>
+                { this.renderDice(['d2','d4','d6','d8']) }
                 </View>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                { renderDice(['d10','d12','d20','d100']) }
+                <View style={ styles.buttonContainers }>
+                { this.renderDice(['d10','d12','d20','d100']) }
                 </View>
                 <NumberSelect
                     id="modifier"
-                    labelText="modifier"
-                    labelFixed={ true }
+                    showPositive={ true }
+                    defaultValue={ this.state.modifier }
                     min={ -99 }
                     max={ 100 }
                     onChange={ updateModifier }
+                    style={ styles.selectors }
+                    itemStyle={ styles.selectorItem }
                 />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    buttonContainers: {
+        flex: 0,
+        flexDirection: 'row',
+        height: 60,
+        margin: 10
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    selectors: {
+        flex: 0,
+        height: 44,
+        margin: 10
+    },
+    selectorItem: {
+        height: 44
+    }
+})
