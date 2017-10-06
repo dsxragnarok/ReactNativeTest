e38b4c14a82490587273b74c3ed74174a71b64e8
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-06 17:47:35 -0600

    refactor: map messageLog to props

--- src/containers/RollLog.js
+++ src/containers/RollLog.js
@@ -1,9 +1,9 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import LogView from './components/LogView';
+import LogView from '../components/LogView';
 import { actions } from '../actions/log';
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = ({ messageLog }) => ({ items: messageLog });
 const mapDispatchToProps = (dispatch) => bindActionCreators({ ...actions }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(LogView);
