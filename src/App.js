54c53781f83b62b54320941fe38ff03ccb348880
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-05 18:16:27 -0600

    refactor: create simple route between Log and Dicebox

--- src/App.js
+++ src/App.js
@@ -5,25 +5,72 @@ import {
   Text,
   View
 } from 'react-native';
+import { NativeRouter, Route, Link } from 'react-router-native';
 import DiceBox from './containers/DiceBox';
 import LogView from './components/LogView';
 
+const items = [{
+    message: 'You rolled 3d6 for 18 [6, 6, 6]'
+}, {
+    message: 'You rolled 2d12 for 24 [12, 12]'
+}, {
+    message: 'You rolled 2d8 for 16 [8, 8]'
+}];
+const diceroll = () => null;
 
 export default class App extends Component {
     render () {
-        const items = [{
-            message: 'You rolled 3d6 for 18 [6, 6, 6]'
-        }, {
-            message: 'You rolled 2d12 for 24 [12, 12]'
-        }, {
-            message: 'You rolled 2d8 for 16 [8, 8]'
-        }];
-
         return (
-            <View style={{ marginTop: 60 }}>
-                <LogView items={ items } />
-                {/* <DiceBox rollDiceCallback={() => console.log('roll dice')} /> */}
-            </View>
+            <NativeRouter>
+                <View style={ styles.container }>
+                    <View style={ styles.nav }>
+                        <Link
+                            to="/"
+                            underlayColor="#f0f4f7"
+                            style={ styles.navItem }
+                        >
+                            <Text>Log</Text>
+                        </Link>
+                        <Link
+                            to="/dicebox"
+                            underlayColor="#f0f4f7"
+                            style={ styles.navItem }
+                        >
+                            <Text>Dice Box</Text>
+                        </Link>
+                    </View>
+                    <Route
+                        exact path="/"
+                        render={ (props) => <LogView { ...props } items={ items } /> }
+                    />
+                    <Route
+                        path="/dicebox"
+                        render={ (props) => <DiceBox { ...props } rollDiceCallback={ diceroll } /> }
+                    />
+                </View>
+            </NativeRouter>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 60,
+        padding: 10
+    },
+    header: {
+        fontSize: 20
+    },
+    nav: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    navItem: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10
+    },
+    subNavItem: {
+        padding: 5
+    },
+});
